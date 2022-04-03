45723842de63604293692302dd5305bd4982238b
Larry Reid <lcreid@jadesystems.ca>
2022-04-03 09:35:30 -0700

    Bundle gem creation

---
+++ lib/mutual_funds.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mutual_funds/version"
+
+module MutualFunds
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "mutual_funds"
+
+require "minitest/autorun"
